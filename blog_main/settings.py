9648959c4ec8f4745beed5f254ef303f439ceee6
Alexandr <alexandr.rofficial@gmail.com>
2026-02-07 17:02:37 +0200

    Version 1.0.1 "Docker Version with DB"

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -79,12 +79,11 @@ WSGI_APPLICATION = "blog_main.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
